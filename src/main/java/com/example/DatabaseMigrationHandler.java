b77c844409c50e10db62ad5221fe5ffbebb85590
nbekenov <nur.beken.usa@gmail.com>
2025-01-05 13:39:30 -0500

    update code for lambda implementation

--- src/main/java/com/example/DatabaseMigrationHandler.java
+++ src/main/java/com/example/DatabaseMigrationHandler.java
@@ -1,59 +1,70 @@
 package com.example;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.flywaydb.core.Flyway;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.postgresql.ssl.NonValidatingFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.util.Objects;
+
 import software.amazon.jdbc.PropertyDefinition;
 import software.amazon.jdbc.ds.AwsWrapperDataSource;
 
-public class DatabaseMigrationHandler {
+import java.util.Properties;
+import java.util.Objects;
+import java.util.Map;
+
+public class DatabaseMigrationHandler implements RequestHandler<Map<String, String>, String> {
+
     // instance vars
     private final String dbHost;
     private final String dbPort;
     private final String dbName;
     private final String dbSchema;
     private final String dbUser;
-    private final String dbPassword;
 
-    private static final String DB_HOST = "localhost";
-    private static final String DB_PORT = "5432";
-    private static final String DB_NAME = "postgres";
-    private static final String DB_SCHEMA = "myschema";
-    private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "mysecretpassword";
+    // property names
+    static final String PROP_DB_HOST = "db.host";
+    static final String PROP_DB_PORT = "db.port";
+    static final String PROP_DB_NAME = "db.name";
+    static final String PROP_DB_SCHEMA = "db.schema";
+    static final String PROP_DB_USER = "db.user";
 
     // Constructor
     public DatabaseMigrationHandler() {
-        this.dbHost = DB_HOST;
-        this.dbPort = DB_PORT;
-        this.dbName = DB_NAME;
-        this.dbSchema = DB_SCHEMA;
-        this.dbUser = DB_USER;
-        this.dbPassword = DB_PASSWORD;
+        this.dbHost = getSystemProperty(PROP_DB_HOST);
+        this.dbPort = getSystemProperty(PROP_DB_PORT);
+        this.dbName = getSystemProperty(PROP_DB_NAME);
+        this.dbSchema = getSystemProperty(PROP_DB_SCHEMA);
+        this.dbUser = getSystemProperty(PROP_DB_USER);
     }
 
+    // method
     private String getSystemProperty(String propName){
         String err_msg = "Required system property '" + propName + "' not found";
         return Objects.requireNonNull(System.getProperty(propName), err_msg);
     }
 
-    public static void main(String[] args) {
+    @Override
+    public String handleRequest(Map<String, String> input, Context context) {
         DatabaseMigrationHandler handler = new DatabaseMigrationHandler();
+        AwsWrapperDataSource dataSource = getDataSource();
         // Test database connection
-        if (handler.testConnection()) {
+        if (testConnection(dataSource)) {
             System.out.println("Database connection successful!");
 
             // Run Flyway migrations
-            handler.runMigrations();
+            runMigrations(dataSource);
         } else {
             System.out.println("Failed to connect to the database.");
         }
+
+        return "ok";
     }
 
-    private boolean testConnection() {
-        try (Connection connection = getDataSource().getConnection()) {
+    private boolean testConnection(AwsWrapperDataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
             return connection != null;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -61,13 +72,15 @@ public class DatabaseMigrationHandler {
         }
     }
 
-    private void runMigrations() {
+    private void runMigrations(AwsWrapperDataSource dataSource) {
         try{
             Flyway flyway = Flyway.configure()
-                    .dataSource(getDataSource())
+                    .dataSource(dataSource)
+                    .createSchemas(true)
                     .schemas(this.dbSchema)
                     .load();
             flyway.migrate();
+
             System.out.println("Completed Database migration!");
         } catch (Exception e) {
             System.out.println("Database migration failed!");
@@ -78,11 +91,13 @@ public class DatabaseMigrationHandler {
     private AwsWrapperDataSource getDataSource() {
         Properties targetDataSourceProps = new Properties();
         targetDataSourceProps.setProperty("ssl", "false");
-        targetDataSourceProps.setProperty("password", this.dbPassword);
+        targetDataSourceProps.setProperty("sslfactory", NonValidatingFactory.class.getName());
+        // Enable AWS IAM database authentication and configure driver property values
+        targetDataSourceProps.setProperty(PropertyDefinition.PLUGINS.name, "iam");
 
         AwsWrapperDataSource ds = new AwsWrapperDataSource();
-        ds.setJdbcProtocol("jdbc:postgresql:");
-        ds.setTargetDataSourceClassName("org.postgresql.ds.PGSimpleDataSource");
+        ds.setJdbcProtocol("jdbc:aws-wrapper:postgresql:");
+        ds.setTargetDataSourceClassName(PGSimpleDataSource.class.getName());
         ds.setServerName(this.dbHost);
         ds.setDatabase(this.dbName);
         ds.setServerPort(this.dbPort);
